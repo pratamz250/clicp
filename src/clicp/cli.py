acbc346721df7dd15bb7055a4a0fe29c1348ee8a
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-05-25 20:18:27 -0300

    Typer inicial

--- src/clicp/cli.py
+++ src/clicp/cli.py
@@ -1,5 +1,13 @@
-def main():
-    print("Hello world")
+import typer
+from clicp.commands.hello import hello
+from clicp.commands.stats import stats
+from clicp.commands.add import add
 
-if __name__ == "main":
-    main()
+app = typer.Typer(no_args_is_help=True)
+
+app.command()(hello)
+app.command()(stats)
+app.command()(add)
+
+def main():
+    app()
